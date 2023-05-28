3f38672286173e09aa0fe2d687deb616ad7d4e50
Documenter.jl <documenter@juliadocs.github.io>
2023-05-28 23:00:47 +0000

    build based on 7892cdc

---
+++ v0.3.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QDScan","category":"page"},{"location":"#QDScan","page":"Home","title":"QDScan","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QDScan.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QDScan]","category":"page"}]
+}
